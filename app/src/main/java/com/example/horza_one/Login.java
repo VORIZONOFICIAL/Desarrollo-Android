f30f9bc9801bb6cd74dd60b22cebc576b0a55080
GrecoAveitua <aveituavilledamaximogreco@gmail.com>
2025-10-14 23:45:29 -0600

    "Login"

--- app/src/main/java/com/example/horza_one/Login.java
+++ app/src/main/java/com/example/horza_one/Login.java
@@ -1,6 +1,8 @@
 package com.example.horza_one;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    Button botonR;
+    EditText nombre, contra, curp, rfc, dir, correo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        botonR = findViewById(R.id.btnCR);
+
+
     }
 }
--- app/src/main/java/com/example/horza_one/Panel_selec.java
+++ app/src/main/java/com/example/horza_one/Panel_selec.java
@@ -1,6 +1,9 @@
 package com.example.horza_one;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Panel_selec extends AppCompatActivity {
+public class Panel_selec extends AppCompatActivity implements View.OnClickListener {
+
+    Button botonr, botonis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_panel_selec);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        botonis = findViewById(R.id.btnis);
+        botonr = findViewById(R.id.btnregistro);
+
+        botonis.setOnClickListener(this);
+        botonr.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intento;
+        if (v.getId() == R.id.btnregistro){
+            intento = new Intent(Panel_selec.this, Login.class);
+            startActivity(intento);
+        } else
+            if (v.getId() == R.id.btnis){
+                intento = new Intent(Panel_selec.this, Login.class);
+                startActivity(intento);
+            }
     }
 }
